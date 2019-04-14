8eb773e851c4dab76073746b04bd20d11f66125f
lilozzy <pc465690@gmail.com>
2019-04-14 02:27:56 +0100

    Initial Commit

---
+++ TvPlays/TvPlays/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TvPlays.Startup))]
+namespace TvPlays
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
