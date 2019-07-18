04561f2ce2c56aa5f5839125b78166ba20153646
lilozzy <pc465690@gmail.com>
2019-07-18 14:47:32 +0100

    Criar Clip - View e Controller Editada

--- TvPlays/TvPlays/App_Start/IdentityConfig.cs
+++ TvPlays/TvPlays/App_Start/IdentityConfig.cs
@@ -35,6 +35,8 @@ namespace TvPlays
     // Configure the application user manager used in this application. UserManager is defined in ASP.NET Identity and is used by the application.
     public class ApplicationUserManager : UserManager<ApplicationUser>
     {
+      
+
         public ApplicationUserManager(IUserStore<ApplicationUser> store)
             : base(store)
         {
